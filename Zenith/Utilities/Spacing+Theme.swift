13edc2a2884ef2786a4609174a9d356d3855e4b2
ironsha-test <test@ironsha>
2026-03-13 14:45:43 -0400

    [ironsha-happy-6e61b333b5a3] test fixture changes

--- Zenith/Utilities/Spacing+Theme.swift
+++ Zenith/Utilities/Spacing+Theme.swift
@@ -32,6 +32,8 @@ extension CGFloat {
     static let progressBarHeight: CGFloat = 14
     static let calorieCardHeight: CGFloat = 200
     static let photoPreviewMaxHeight: CGFloat = 300
+    static let datePickerSheetHeight: CGFloat = 460
+    static let dashboardHeaderEstimate: CGFloat = 130
 
     // MARK: - Input Fields
 
